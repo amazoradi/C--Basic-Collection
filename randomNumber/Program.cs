364e622ffb108bf34999194d0dfd99969a305183
Austin Zoradi <amazoradi@gmail.com>
2019-03-04 11:21:11 -0600

    random numbers complete

---
+++ randomNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace randomNumber
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Random random = new Random();
+      List<int> numbers = new List<int> {
+        random.Next(10),
+        random.Next(10),
+        random.Next(10),
+        random.Next(10),
+        random.Next(10),
+      };
+
+      numbers.ForEach(num => Console.WriteLine(num));
+
+      for (int i = 0; i < (numbers.Count); i++)
+      {
+       if (numbers.Contains(i))
+        {
+          Console.WriteLine("Number list contains: {0}", i);
+        }
+        else
+        {
+          Console.WriteLine("Number list  does NOT contains: {0}", i);
+        }
+      }
+    }
+  }
+}
